856da35fd6d782a9ba83400007e038f5a040d711
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-05-18 15:29:58 -0300

    criando as custons cell

---
+++ MyWeather/MyWeather/HourlyCollectionCell/HourlyTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  HourlyTableViewCell.swift
+//  MyWeather
+//
+//  Created by Daniel de Andrade Souza on 18/05/22.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    static let identifier = "HourlyTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "HourlyTableViewCell", bundle: nil)
+    }
+    
+}
