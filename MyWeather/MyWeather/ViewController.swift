7903549822512fe9843217c9d8eb6b8a88573c82
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-05-18 15:23:07 -0300

    implementando a viewcontroller

--- MyWeather/MyWeather/ViewController.swift
+++ MyWeather/MyWeather/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Location: CoreLocation
+// table view
+// custom cell: collection view
+// API / request to get the data
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    var models = [Weather]()
 
     override func viewDidLoad() {
+        
+        // Register 2 cells
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
 
+    // table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
 
 }
 
+struct Weather {
+    
+}
